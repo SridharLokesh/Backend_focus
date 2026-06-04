f0c04a1728c69e8c03b6981f750e219b910e31df
Lokesh S <lokeshsridhar2002@gmail.com>
2026-06-04 22:34:47 +0530

    updated cors url

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.use(cors({
     'http://localhost:5173',
     'http://localhost:3000',
     'http://127.0.0.1:5173',
+    'https://e-comfe.vercel.app/',
     process.env.FRONTEND_URL,
   ].filter(Boolean),
   credentials: true,
