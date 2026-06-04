7f59df9bfa45eb9d90c6ad2eef736e33b34ece5c
Lokesh S <logesh3736@gmail.com>
2026-06-04 23:07:38 +0530

    url change in cors

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(cors({
     'http://localhost:5173',
     'http://localhost:3000',
     'http://127.0.0.1:5173',
-    'https://e-comfe.vercel.app/',
+    'https://fe-tvs-e-com.vercel.app',
     process.env.FRONTEND_URL,
   ].filter(Boolean),
   credentials: true,
