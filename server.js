c1ad2d97e1097e3fb4b4bfb605d171309a48bdd4
Lokesh S <logesh3736@gmail.com>
2026-06-11 10:36:15 +0530

    updated api.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(cors({
     'http://localhost:5173',
     'http://localhost:3000',
     'http://127.0.0.1:5173',
-    'https://e-com-automobiles.vercel.app',
+    'https://e-comauto.vercel.app',
     process.env.FRONTEND_URL,
   ].filter(Boolean),
   credentials: true,
